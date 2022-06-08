9454960f685393a84ba122c150dbba6f8fc54632
HEltim7 <heltimc@gmail.com>
2022-06-08 21:57:23 +0800

    E

--- CF/Round/797/E_Price_Maximization.cpp
+++ CF/Round/797/E_Price_Maximization.cpp
@@ -1,38 +1,36 @@
 #include<vector>
 #include<iostream>
 #include<algorithm>
+#include<set>
 using namespace std;
 
 #define endl '\n'
 using LL=long long;
-const int N=2e5+10,K=1e3+10;
-int arr[K];
+const int N=2e5+10;
 
 void solve() {
     int n,k;
     cin>>n>>k;
-    LL sum=0;
+    multiset<int> st;
+    LL ans=0;
     for(int i=1;i<=n;i++) {
         int in;
         cin>>in;
-        arr[in%k]++;
-        sum+=in;
+        ans+=in/k;
+        st.insert(in%k);
     }
-    for(int i=0;i<k;i++)
-        for(int j=0;j<k;j++) {
-            int r=(k-j+i)%k;
-            if(j==r) {
-                int t=arr[j]/2;
-                sum-=1LL*t*i;
-                arr[j]-=t;
-            }
-            else {
-                int t=min(arr[j],arr[r]);
-                sum-=1LL*t*i;
-                arr[j]-=t,arr[r]-=t;
-            }
+    while(st.size()) {
+        int cur=*st.begin();
+        st.erase(st.begin());
+        int aim=k-cur;
+        auto it=st.lower_bound(aim);
+        if(it!=st.end()) {
+            if(cur+*it>=k) ans++;
+            st.erase(it);
         }
-    cout<<sum/k<<endl;
+        else st.erase(st.begin());
+    }
+    cout<<ans<<endl;
 }
 
 int main() {
