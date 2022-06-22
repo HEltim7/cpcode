ffca2804023ea3d2da285f188bf601e39d7432b0
HEltim7 <heltimc@gmail.com>
2022-06-22 21:04:49 +0800

    cf

--- CF/Round/800/E_Keshi_in_Search_of_AmShZ.cpp
+++ CF/Round/800/E_Keshi_in_Search_of_AmShZ.cpp
@@ -2,19 +2,38 @@
 #include<iostream>
 #include<algorithm>
 #include<queue>
+#include<cstring>
 using namespace std;
 
 #define endl '\n'
 using LL=long long;
-
+using PII=pair<int,int>;
 const int N=2e5+10;
-int fa[N],dis[N],mark[N];
+int ind[N],dis[N];
 vector<int> adj[N];
-pair<int,int> edge[N];
+bool mark[N];
 
-int findfa(int x) {
-    if(fa[x]==x) return x;
-    return fa[x]=findfa(fa[x]);
+int dijkstra(int n) {
+    priority_queue<PII,vector<PII>,greater<PII>> heap;
+    memset(dis,0x3f,sizeof (int)*(n+1));
+    dis[n]=0;
+    heap.emplace(dis[n],n);
+    while(heap.size()) {
+        auto [d,u]=heap.top();
+        heap.pop();
+        if(u==1) return dis[u];
+        if(mark[u]) continue;
+        mark[u]=1;
+        for(int v:adj[u]) {
+            int cur=dis[u]+ind[v];
+            if(cur<dis[v]) {
+                dis[v]=cur;
+                heap.emplace(cur,v);
+            }
+            ind[v]--;
+        }
+    }
+    return -1;
 }
 
 int main() {
@@ -22,34 +41,12 @@ int main() {
     cin.tie(nullptr);
     int n,m;
     cin>>n>>m;
-    for(int i=1;i<=n;i++) fa[i]=i;
     for(int i=1;i<=m;i++) {
         int a,b;
         cin>>a>>b;
-        edge[i]={b,a};
-        adj[b].push_back(a);
+        adj[b].emplace_back(a);
+        ind[a]++;
     }
-    int ans=m,res=0;
-    queue<int> q;
-    q.push(n);
-    while(q.size()) {
-        int u=q.front();
-        q.pop();
-        for(auto v:adj[u]) {
-            if(findfa(v)!=findfa(u)) {
-                ans--;
-                fa[u]=findfa(v);
-                dis[v]=max(dis[v],dis[u]+1);
-                if(v==1) res=max(res,dis[v]);
-                if(!mark[v]) q.push(v);
-                mark[v]=1;
-            }
-        }
-    }
-    ans+=res;
-    for(int i=1;i<=m;i++) {
-        if(!mark[edge[i].second]) ans--;
-    }   
-    cout<<ans<<endl;
+    cout<<dijkstra(n)<<endl;
     return 0;
 }
