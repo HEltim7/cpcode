3880d423b5066b646b9c6a6174503491424d28fd
HEltim7 <heltimc@gmail.com>
2022-07-26 21:18:28 +0800

    hdu

--- Archive/HDU/多校2022/contest3/1002.cpp
+++ Archive/HDU/多校2022/contest3/1002.cpp
@@ -1,3 +1,5 @@
+#pragma GCC optimize("Ofast")
+#include <cstdio>
 #include <cstring>
 #include <vector>
 #include <iostream>
@@ -5,28 +7,116 @@
 using namespace std;
 
 #define endl '\n'
-#define lowbit(x) (x&(-x))
 using LL=long long;
 const int N=1e5+10,M=20;
-LL H;
-int n,full;
+int n;
 int tim[M],len[M];
-int pre[M][N];
 int sttime[1<<M];
-vector<int> st[M];
+LL H;
+LL pre[M][N];
 LL dp[1<<M];
+vector<int> st[M];
 
-int cntbit(int x) {
-    int res=0;
-    while(x) x-=lowbit(x),res++;
-    return res;
+namespace io {
+    const int MAXBUF = 1e6, MAXLEN = 1e6;
+    char buf[MAXBUF], *pl, *pr;
+    char str[MAXLEN];
+
+    #define gc() \
+    (pl == pr && (pr = (pl = buf) + fread(buf, 1, MAXBUF, stdin), pl == pr) \
+    ? EOF : *pl++)
+
+    template<typename T> T rd(T &x) {
+        x = 0;
+        T f = 1;
+        char c = gc();
+        while (!isdigit(c)) {
+            if (c == '-') f = -1;
+            c = gc();
+        }
+        while (isdigit(c)) x = x * 10 + (c ^ 48), c = gc();
+        if (c != '.') return x = x * f;
+        for (double t = 0.1; c = gc(), isdigit(c); t *= 0.1) x += (c - '0') * t;
+        return x = x * f;
+    }
+
+    char* rd(char *p = str) {
+        char c = gc(), *h = p;
+        while (!isgraph(c)) c = gc();
+        while (isgraph(c)) *p++ = c, c = gc();
+        *p = '\0';
+        return h;
+    }
+
+    char rd(char &c) {
+        c = gc();
+        while (!isgraph(c)) c = gc();
+        return c;
+    }
+
+    string rd(string &s) { return s = rd(str); }
+
+    // template<typename... T> void reads_impl(T&... x) { (rd(x),...); }
+
+    #define read(x) io::rd(x)
+    // #define reads(...) io::reads_impl(__VA_ARGS__)
+
+    const int MAXPBUF = 1e6, PRECISION = 7;
+    char pbuf[MAXPBUF], *pp = pbuf;
+
+    void clear_buffer() { fwrite(pbuf, 1, pp-pbuf, stdout), pp = pbuf; }
+
+    void push(const char &c) {
+        if (pp - pbuf == MAXPBUF) clear_buffer();
+        *pp++ = c;
+    }
+
+    template<typename T> void wt(T x) {
+        if (x < 0) push('-'), x = -x;
+        static int sta[40];
+        int top = 0;
+        do {
+            sta[top++] = x % 10;
+        } while (x/=10);
+        while (top) push(sta[--top] + '0');
+    }
+
+    template<typename T> void wt_f(T x,int p) {
+        if (x < 0) push('-'), x = -x;
+        long long pre = (long long)x;
+        wt(pre);
+        x -= pre;
+        if (p) push('.');
+        while (p--) {
+            x *= 10;
+            int t = (int)x;
+            x -= t;
+            push(t + '0');
+        }
+    }
+
+    void wt(const char &c) { push(c); }
+    void wt(const string &s) { for (auto &x:s) push(x); }
+    void wt(const char *p) { while (*p != '\0') push(*p++); }
+
+    void wt(const float &x, int p = PRECISION) { wt_f(x,p); }
+    void wt(const double &x, int p = PRECISION) { wt_f(x,p); }
+    void wt(const long double &x, int p = PRECISION) { wt_f(x,p); }
+
+    // template<typename... T> void writes_impl(const T&... x) { (wt(x),...); }
+
+    #define write(...) io::wt(__VA_ARGS__)
+    // #define writes(...) io::writes_impl(__VA_ARGS__)
+
+    struct Exit {
+        ~Exit() { clear_buffer(); }
+    } static exit;
 }
 
 int caltime(int x) {
     int res=0;
-    for(int i=0;i<n;i++) {
+    for(int i=0;i<n;i++)
         if(x&(1<<i)) res+=tim[i];
-    }
     return res;
 }
 
@@ -48,38 +138,36 @@ bool check(int mid) {
 }
 
 void solve() {
-    cin>>n>>H;
+    read(n),read(H);
     LL sum=0;
+    int l=1,r=0;
     for(int i=0;i<n;i++) {
-        cin>>tim[i]>>len[i];
+        read(tim[i]),read(len[i]);
+        r+=max(tim[i],len[i]);
         for(int j=1;j<=len[i];j++) {
-            cin>>pre[i][j];
+            read(pre[i][j]);
             pre[i][j]+=pre[i][j-1];
         }
         sum+=pre[i][len[i]];
     }
     if(sum<H) {
-        cout<<-1<<endl;
+        write("-1\n");
         return;
     }
-    for(int i=0;i<(1<<n);i++) st[cntbit(i)].push_back(i);
+    for(int i=0;i<=n;i++) st[i].clear();
+    for(int i=0;i<(1<<n);i++) st[__builtin_popcount(i)].push_back(i);
     for(int i=0;i<(1<<n);i++) sttime[i]=caltime(i);
-    full=(1<<n)-1;
     
-    int l=1,r=2e6;
     while(l<r) {
         int mid=l+r>>1;
         if(check(mid)) r=mid;
         else l=mid+1;
     }
-    cout<<l-1<<endl;
+    write(l-1),write('\n');
 }
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(nullptr);
-    int t;
-    cin>>t;
+    int t=read(t);
     while(t--) solve();
     return 0;
 }
