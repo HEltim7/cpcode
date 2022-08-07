9007484b4cc00fad21c69fe1599cb9db9be3a05f
HEltim7 <heltimc@gmail.com>
2022-08-07 17:03:07 +0800

    sam

--- Luogu/Problems/6640.cpp
+++ Luogu/Problems/6640.cpp
@@ -1,3 +1,4 @@
+#include <string>
 #include <vector>
 #include <iostream>
 #include <algorithm>
@@ -5,6 +6,30 @@ using namespace std;
 
 #define endl '\n'
 using LL=long long;
+const int N=2e5+10,M=20;
+int len[N],r[N];
+int stl[M][N],str[M][N];
+
+void get_st(int n) {
+    for(int i=1;i<=n;i++) stl[0][i]=len[i],str[0][i]=r[i];
+    for(int k=1;k<M;k++) {
+        int t=1<<k;
+        for(int i=1,j=i+t-1,mid=i+t/2;j<=n;i++,j++,mid++) {
+            stl[k][i]=max(stl[k-1][i],stl[k-1][mid]);
+            str[k][i]=max(str[k-1][i],str[k-1][mid]);
+        }
+    }
+}
+
+int query_len(int l,int r) {
+    int k=__lg(r-l+1);
+    return max(stl[k][l],stl[k][r-(1<<k)+1]);
+}
+
+int query_r(int l,int r) {
+    int k=__lg(r-l+1);
+    return max(str[k][k],str[k][r-(1<<k)+1]);
+}
 
 struct SuffixAutomaton {
     const static int A=26;
@@ -45,19 +70,41 @@ struct SuffixAutomaton {
         }
     }
 
+    void match(string &s) {
+        int idx=1,u=0;
+        for(auto x:s) {
+            int c=x-B;
+            len[idx]=len[idx-1];
+            while(u&&!edp[u].ch[c]) u=edp[u].link,len[idx]=edp[u].len;
+            if(edp[u].ch[c]) u=edp[u].ch[c],len[idx]++;
+            r[idx-len[idx]+1]=idx;
+            idx++;
+        }
+    }
+
     void build(string &s) { for(auto x:s) extend(x); }
     void clear() { edp.clear(),edp.push_back({-1}),last=0; }
     int size() { return edp.size(); }
     
     SuffixAutomaton() { edp.push_back({-1}); }
     SuffixAutomaton(int sz) { edp.reserve(sz),edp.push_back({-1}); }
-} sam;
+} sam(N*2);
 
 void solve() {
     string s,t;
     int q;
     cin>>s>>t>>q;
-    
+    sam.build(t);
+    sam.match(s);
+    get_st(s.size());
+
+    while(q--) {
+        int l,r;
+        cin>>l>>r;
+        int res=0;
+        res=max(res,min(r,query_r(1,l))-l+1);
+        res=max(res,query_len(l,r));
+    }
 }
 
 int main() {
--- NowCoder/暑假/2022/contest3/H.cpp
+++ NowCoder/暑假/2022/contest3/H.cpp
@@ -1,21 +1,32 @@
-#include <string>
 #include <vector>
 #include <iostream>
 #include <algorithm>
-#include <queue>
 using namespace std;
 
 #define endl '\n'
 using LL=long long;
-const int N=2e6+10;
-bool mark[N];
+const int N=1e5+10,M=18;
+LL val[N],pre[N],st[M][N];
+
+void get_st(int n) {
+    for(int i=1;i<=n;i++) st[0][i]=pre[i];
+    for(int k=1;k<M;k++) {
+        int len=1<<k;
+        for(int i=1,mid=i+len/2,j=1+len-1;j<=n;i++,mid++,j++)
+            st[k][i]=min(st[k-1][i],st[k-1][mid]);
+    }
+}
+
+LL query(int l,int r) {
+    int k=__lg(r-l);
+    return min(st[k][l],st[k][r-(1<<k)+1]);
+}
 
 struct SuffixAutomaton {
     const static int A=26;
     const static char B='a';
     struct Endpos {
         int link,len;
-        int ind,siz;
         int ch[A];
     };
     vector<Endpos> edp;
@@ -50,54 +61,32 @@ struct SuffixAutomaton {
         }
     }
 
-    void get_siz(string &s) {
-        for(int i=0,u=0;i<s.size();i++) u=edp[u].ch[s[i]-B],edp[u].siz++;
-        queue<int> q;
-        for(int i=1;i<size();i++) edp[edp[i].link].ind++;
-        for(int i=1;i<size();i++) if(!edp[i].ind) q.push(i);
-        while(q.size()) {
-            int u=q.front();
-            q.pop();
-            edp[edp[u].link].siz+=edp[u].siz;
-            if(edp[u].link&&!--edp[edp[u].link].ind) q.push(edp[u].link);
-        }
-    }
-
-    int match(string &s) {
-        vector<int> arr;
-        int ans=0,u=0,len=0,n=s.size();
-        s+=s;
-        s.pop_back();
+    LL match(string &s) {
+        LL ans=0;
+        int u=0,len=0,idx=1;
         for(auto x:s) {
             int c=x-B;
             while(u&&!edp[u].ch[c]) u=edp[u].link,len=edp[u].len;
             if(edp[u].ch[c]) u=edp[u].ch[c],len++;
-            if(len==n) {
-                if(!mark[u]) ans+=edp[u].siz,arr.push_back(u);
-                mark[u]=1;
-                if(edp[edp[u].link].len==len-1) u=edp[u].link;
-                len--;
-            }
+            ans=max(ans,)
         }
-        for(auto x:arr) mark[x]=0;
-        return ans;
     }
 
-    void build(string &s) { for(auto x:s) extend(x); get_siz(s); }
+    void build(string &s) { for(auto x:s) extend(x); }
     void clear() { edp.clear(),edp.push_back({-1}),last=0; }
     int size() { return edp.size(); }
     
     SuffixAutomaton() { edp.push_back({-1}); }
     SuffixAutomaton(int sz) { edp.reserve(sz),edp.push_back({-1}); }
-} sam(N);
+} sam(N*2);
 
 void solve() {
+    int n,k,m;
     string s;
-    cin>>s;
+    cin>>n>>k>>m>>s;
     sam.build(s);
-    int n;
-    cin>>n;
-    for(int i=1;i<=n;i++) {
+    for(int i=1;i<=n;i++) cin>>val[i],pre[i]=val[i]+pre[i-1];
+    while(k--) {
         cin>>s;
         cout<<sam.match(s)<<endl;
     }
