e6997936bfb5da72aede6fb8ba59756445d9db89
HEltim7 <heltimc@gmail.com>
2023-09-08 22:45:50 +0800

    sgt merging

--- Luogu/P4197.cpp
+++ Luogu/P4197.cpp
@@ -14,78 +14,60 @@ using namespace std;
 #define endl '\n'
 using LL=long long;
 constexpr int N=1e5+10;
-int h[N];
 
-struct MergeSplitSegmentTree {
-
-    #define lch tr[u].ch[0]
-    #define rch tr[u].ch[1]
-    constexpr static int MAX_SIZE=1e7+10;
+namespace sgt {
+    #define lch (tr[u].lc)
+    #define rch (tr[u].rc)
     
     struct Node {
-        int ch[2];
-        int cnt;
-    } tr[MAX_SIZE];
+        int lc=0,rc=0;
+        int cnt=0;
+    };
+    constexpr int M=N*__lg(N)*2;
+    vector<Node> tr(M);
     int idx;
 
     int new_node() {
-        // assert(idx<MAX_SIZE);
-        return ++idx;
+        assert(++idx<M);
+        tr[idx]={};
+        return idx;
     }
 
     void pushup(int u) {
-        if(lch&&rch) ;
-        else if(lch) ;
-        else if(rch) ;
+        tr[u].cnt=tr[lch].cnt+tr[rch].cnt;
     }
 
-    // remember to pushdn laze tag
     void pushdn(int u) {
-        if(lch) ;
-        if(rch) ;
-
+        
     }
     
-    void merge(int &u,int v) {
-        if(!u&&!v) return;
+    void merge(int &u,int v,int l,int r) {
         if(!u||!v) u=u|v;
+        else if(l==r) tr[u].cnt+=tr[v].cnt;
         else {
-            pushdn(u);pushdn(v);
-            merge(lch,tr[v].ch[0]);
-            merge(rch,tr[v].ch[1]);
+            pushdn(u),pushdn(v);
+            int mid=(l+r)/2;
+            merge(tr[u].lc, tr[v].lc, l, mid);
+            merge(tr[u].rc, tr[v].rc, mid+1, r);
             pushup(u);
-        }
-    }
-
-    // k][k+1
-    void split(int &u,int &v,int l,int r,int k) {
-        if(!u||k>=r) return;
-        if(k<l) swap(u,v);
-        else {
-            v=new_node();
-            int mid=l+r>>1;
-            if(k<=mid) swap(rch,tr[v].ch[1]);
-            pushdn(u);
-            if(k<mid) split(lch, tr[v].ch[0], l, mid, k);
-            else split(rch, tr[v].ch[1], mid+1, r, k);
-            pushup(u),pushup(v);
         }
     }
 
     int kth(int u,int l,int r,int k) {
         if(tr[u].cnt<k) return -1;
         if(l==r) return l;
-        int mid=l+r>>1;
         pushdn(u);
+        int mid=(l+r)/2;
         if(tr[lch].cnt>=k) return kth(lch, l, mid, k);
         return kth(rch, mid+1, r, k-tr[lch].cnt);
     }
 
     void build(int &u,int l,int r,int p) {
-        u=new_node();
-        if(l==r) ;
+        if(!u) u=new_node();
+        if(l==r) tr[u].cnt++;
         else {
-            int mid=l+r>>1;
+            pushdn(u);
+            int mid=(l+r)/2;
             if(p<=mid) build(lch,l,mid,p);
             else build(rch,mid+1,r,p);
             pushup(u);
@@ -94,16 +76,94 @@ struct MergeSplitSegmentTree {
 
     #undef lch
     #undef rch
+};
 
-} sgt;
+struct DisjointUnionSet {
+    vector<int> fa,sz,rt;
+
+    void init(int n) {
+        fa.resize(n+1);
+        sz.assign(n+1,1);
+        rt.resize(n+1);
+        iota(fa.begin(), fa.end(), 0);
+    }
+
+    int find(int x) {
+        return x==fa[x]?x:fa[x]=find(fa[x]);
+    }
+
+    bool same(int x,int y) {
+        return find(x)==find(y);
+    }
+
+    bool join(int x,int y) {
+        x=find(x);
+        y=find(y);
+        if(x==y) return false;
+        if(sz[x]<sz[y]) swap(x,y);
+        sz[x]+=sz[y];
+        fa[y]=x;
+        sgt::merge(rt[x], rt[y], 0, N);
+        return true;
+    }
+
+    int size(int x) {
+        return sz[find(x)];
+    }
+
+    int root(int x) {
+        return rt[find(x)];
+    }
+
+    DisjointUnionSet() = default;
+    DisjointUnionSet(int n) { init(n); }
+} dsu;
 
 void solve() {
     int n,m,q;
     cin>>n>>m>>q;
-    for(int i=1;i<=n;i++) cin>>h[i];
-    for(int i=1;i<=m;i++) {
-        
+    vector<int> h(n+1),num;
+    for(int i=1;i<=n;i++) cin>>h[i],num.emplace_back(h[i]);
+    sort(num.begin(),num.end());
+    num.erase(unique(num.begin(),num.end()),num.end());
+    for(int i=1;i<=n;i++) {
+        h[i]=lower_bound(num.begin(),num.end(),h[i])-num.begin();
     }
+
+    vector<int> ans(q);
+    vector<tuple<int,int,int>> edg(m);
+    vector<tuple<int,int,int,int>> qry(q);
+    for(auto &[c,a,b]:edg) cin>>a>>b>>c;
+    for(int i=0;i<q;i++) {
+        auto &[x,v,k,id]=qry[i];
+        cin>>v>>x>>k;
+        id=i;
+    }
+    sort(edg.begin(),edg.end());
+    sort(qry.begin(),qry.end());
+
+    DisjointUnionSet dsu(n);
+    for(int i=1;i<=n;i++) {
+        sgt::build(dsu.rt[i], 0, N, h[i]);
+    }
+
+    int idx=0;
+    for(auto [x,v,k,id]:qry) {
+        while(idx<m) {
+            auto [c,a,b]=edg[idx];
+            if(c>x) break;
+            dsu.join(a, b);
+            idx++;
+        }
+        if(k<=0||k>dsu.size(v)) ans[id]=-1;
+        else {
+            k=dsu.size(v)-k+1;
+            int rt=dsu.root(v);
+            ans[id]=num[sgt::kth(rt, 0, N, k)];
+        }
+    }
+
+    for(int x:ans) cout<<x<<endl;
 }
 
 int main() {
--- Luogu/P5494.cpp
+++ Luogu/P5494.cpp
@@ -1,134 +1,165 @@
-#include <utility>
-#include <vector>
-#include <iostream>
 #include <algorithm>
+#include <array>
 #include <cassert>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <tuple>
+#include <vector>
 using namespace std;
 
 #define endl '\n'
 using LL=long long;
-const int N=2e5+10,M=36*N;
+constexpr int N=2e5+10;
 
-// #define ONLINE_JUDGE
-#ifndef ONLINE_JUDGE
-#include <heltim7/debug>
-#else
-#define debug(...)
-#endif
+namespace sgt {
+    #define lch (tr[u].lc)
+    #define rch (tr[u].rc)
 
-int root[N],id,idx;
+    struct Node {
+        int lc=0,rc=0;
+        LL cnt=0;
+    };
+    constexpr int M=N*(__lg(N)+1)*2;
+    vector<Node> tr(M);
+    int idx=0;
 
-struct Node {
-    int l,r;
-    LL cnt;
-} tr[M];
+    int new_node() {
+        assert(++idx<M);
+        tr[idx]={};
+        return idx;
+    }
 
-int new_node() {
-    assert(idx<M);
-    return ++idx;
-}
+    void pushup(int u) {
+        tr[u].cnt=tr[lch].cnt+tr[rch].cnt;
+    }
 
-void pushup(int u) {
-    assert(u);
-    tr[u].cnt=0;
-    if(tr[u].l) tr[u].cnt+=tr[tr[u].l].cnt;
-    if(tr[u].r) tr[u].cnt+=tr[tr[u].r].cnt;
-}
+    void update(int u) {
+        if(!u) return;
 
-void modify(int u,int l,int r,int p,int v) {
-    if(l==r) tr[u].cnt+=v;
-    else {
-        int mid=l+r>>1;
-        if(p<=mid) {
-            if(!tr[u].l) tr[u].l=new_node();
-            modify(tr[u].l, l, mid, p, v);
+    }
+
+    void pushdn(int u) {
+
+    }
+
+    int kth(int u,int l,int r,int k) {
+        if(tr[u].cnt<k) return -1;
+        if(l==r) return l;
+        int mid=(l+r)/2;
+        if(tr[lch].cnt>=k) return kth(lch, l, mid, k);
+        return kth(rch, mid+1, r, k-tr[lch].cnt);
+    }
+
+    void merge(int &u,int v,int l,int r) {
+        if(!u||!v) u=u|v;
+        else if(l==r) {
+            tr[u].cnt+=tr[v].cnt;
         }
         else {
-            if(!tr[u].r) tr[u].r=new_node();
-            modify(tr[u].r, mid+1, r, p, v);
+            pushdn(u);
+            int mid=(l+r)/2;
+            merge(tr[u].lc, tr[v].lc, l, mid);
+            merge(tr[u].rc, tr[v].rc, mid+1, r);
+            pushup(u),pushup(v);
         }
-        pushup(u);
     }
-}
 
-//merge v to u
-void merge(int &u,int v) {
-    if(!u||!v) u=u+v;
-    else {
-        tr[u].cnt+=tr[v].cnt;
-        merge(tr[u].l, tr[v].l);
-        merge(tr[u].r, tr[v].r);
-    }
-}
-
-//split u to v [1-k]->u [k+1-INF]->v
-void split(int &u,int &v,int l,int r,int k) {
-    if(!u||k>=r) return;
-    if(k<l) swap(u,v);
-    else {
-        v=new_node();
-        int mid=l+r>>1;
-        if(k<=mid) swap(tr[u].r,tr[v].r);
-        if(k<mid) split(tr[u].l, tr[v].l, l, mid, k);
-        else split(tr[u].r, tr[v].r, mid+1, r, k);
+    pair<int,int> split(int u,int l,int r,int p) {
+        if(r<p) return {u,0};
+        if(l>=p) return {0,u};
+        pushdn(u);
+        int v=new_node();
+        int mid=(l+r)/2;
+        auto [a,b]=split(lch, l, mid, p);
+        auto [c,d]=split(rch, mid+1, r, p);
+        tr[u].lc=a,tr[u].rc=c;
+        tr[v].lc=b,tr[v].rc=d;
         pushup(u),pushup(v);
+        return {u,v};
     }
-}
 
-LL count(int u,int l,int r,int x,int y) {
-    if(!u) return 0;
-    if(l>=x&&r<=y) return tr[u].cnt;
-    int mid=l+r>>1;
-    LL res=0;
-    if(mid>=x) res+=count(tr[u].l,l,mid,x,y);
-    if(mid<y) res+=count(tr[u].r,mid+1,r,x,y);
-    return res;
-}
+    pair<int,int> extract(int u,int l,int r,int x,int y) {
+        auto [a,b]=split(u, l, r, x);
+        auto [c,d]=split(b, l, r, y+1);
+        merge(a, d, l, r);
+        return {a,c};
+    }
 
-int kth(int u,int l,int r,LL k) {
-    if(tr[u].cnt<k) return -1;
-    if(l==r) return l;
-    int mid=l+r>>1;
-    if(tr[tr[u].l].cnt>=k) return kth(tr[u].l, l, mid, k);
-    return kth(tr[u].r, mid+1, r, k-tr[tr[u].l].cnt);
+    LL query(int u,int l,int r,int x,int y) {
+        if(!u||l>y||r<x) return {};
+        if(l>=x&&r<=y) return tr[u].cnt;
+        pushdn(u);
+        int mid=(l+r)/2;
+        return query(lch, l, mid, x, y)+query(rch, mid+1, r, x, y);
+    }
+
+    void modify(int &u,int l,int r,int p,int v) {
+        if(!u) u=new_node();
+        if(l==r) {
+            tr[u].cnt+=v;
+        }
+        else {
+            int mid=(l+r)/2;
+            pushdn(u);
+            if(p<=mid) modify(lch, l, mid, p, v);
+            else modify(rch, mid+1, r, p, v);
+            pushup(u);
+        }
+    }
+
+    #undef lch
+    #undef rch
 }
 
 void solve() {
-    root[++id]=++idx;
     int n,m;
     cin>>n>>m;
+    int idx=1;
+    vector<int> rt(m+2);
     for(int i=1;i<=n;i++) {
         int in;
         cin>>in;
-        modify(root[1], 1, N-1, i, in);
+        sgt::modify(rt[1], 1, n, i, in);
     }
+
     while(m--) {
-        int op,p,x,y;
-        cin>>op>>p>>x;
-        if(op==0||op==2||op==3) cin>>y;
+        int op;
+        cin>>op;
         if(op==0) {
-            int tmp=0;
-            split(root[p], root[++id], 1, N-1, x-1);
-            split(root[id], tmp, 1, N-1, y);
-            merge(root[p],tmp);
+            int p,l,r;
+            cin>>p>>l>>r;
+            tie(rt[p],rt[++idx])=sgt::extract(rt[p], 1, n, l, r);
         }
         else if(op==1) {
-            merge(root[p],root[x]);
+            int p,q;
+            cin>>p>>q;
+            sgt::merge(rt[p], rt[q], 1, n);
         }
         else if(op==2) {
-            modify(root[p], 1, N-1, y, x);
+            int p,x,q;
+            cin>>p>>x>>q;
+            sgt::modify(rt[p], 1, n, q, x);
         }
         else if(op==3) {
-            cout<<count(root[p], 1, N-1, x, y)<<endl;
+            int p,l,r;
+            cin>>p>>l>>r;
+            cout<<sgt::query(rt[p], 1, n, l, r)<<endl;
         }
-        else if(op==4) {
-            cout<<kth(root[p], 1, N-1, x)<<endl;
+        else {
+            int p,k;
+            cin>>p>>k;
+            cout<<sgt::kth(rt[p], 1, n, k)<<endl;
         }
     }
 }
 
 int main() {
-    ios::sync_with_stdio(0);
+    ios::sync_with_stdio(false);
     cin.tie(nullptr);
     solve();
     return 0;
